b77ae388ea1e42a34efbe5d093abf307040cddaa
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-10-15 15:06:08 -0400

    successfully pulls match history
    -- added style sheet for page

--- personal_history.js
+++ personal_history.js
@@ -27,6 +27,7 @@ function match()
     this.kills = 0;
     this.deaths = 0;
     this.assists = 0;
+    this.fetched = false;
 }
 
 // Function returns a random integer between min and max inclusive
@@ -86,22 +87,29 @@ async function pullIndividualMatch(match){
             playerInfo = 0;
             // first find participant == player
             for (let i = 0; i < 10; i++){
-                if (result.info.participants[i].puuid == match.playerId){
+                if (result.info.participants[i].puuid == player.puuid){
                     playerInfo = i;
                 }
             }
             match.victory = result.info.participants[playerInfo].win;
             match.champPlayed = result.info.participants[playerInfo].championName;
-            match.side = result.info.participants[playerInfo].teamid;
+            if (result.info.participants[playerInfo].teamId == 200){
+                match.side = "Red";
+            }
+            else {
+                match.side = "Blue";
+            }
             match.kills = result.info.participants[playerInfo].kills;
             match.assists = result.info.participants[playerInfo].assists;
             match.deaths = result.info.participants[playerInfo].deaths;
+            match.fetched = true;
         })
         .catch(error=>alert(error))
 }
 
 // search for summoner
 async function searchSummoner() {
+    await eliminateExistingMatches();
     summoner_input = document.getElementById("summoner_name");
     player.name = summoner_input.value;
     await pullSummonerID(player);
@@ -110,47 +118,83 @@ async function searchSummoner() {
     {
         await pullIndividualMatch(matchHistory20[i]);
 //        genMatch(i);
-        populateSingleMatch(i);
+        if (matchHistory20[i].fetched == true){
+            populateSingleMatch(i);
+        }
+    }
+}
+
+function eliminateExistingMatches(){
+    removeAllChildNodes(document.getElementById("matchhistory"));
+    return 0;
+}
+
+function removeAllChildNodes(parent){
+    while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
     }
 }
 
 
 // function to populate matches into feed
 function populateSingleMatch(matchNumber){
+    currMatch = matchHistory20[matchNumber];
 
     let div_col_outer = document.createElement("mc_container");
     div_col_outer.classList.add("col-md-6");
 
     let div_row = document.createElement("div");
     div_row.classList.add("row", "g-0", "border", "rounded",
-                        "overflow-hidden", "flex-md-row", "mb-4", "shadow-sm",
-                         "h-md-600", "position-relative");
+                        "overflow-hidden", "flex-md-row", "mb-2", "shadow-sm",
+                         "h-md-300", "position-relative");
+
+
 
     let div_col_inner = document.createElement("div");
-    div_col_inner.classList.add("col", "p-4", "d-flex", "flex-column", "position-static");
+    div_col_inner.classList.add("col", "p-2", "d-flex", "flex-column", "position-relative");
+
+    let div_col_middle = document.createElement("div");
+    div_col_middle.classList.add("col", "p-2", "d-flex", "flex-column", "position-relative");
+
+
 
     let strong_text = document.createElement("strong");
-    strong_text.classList.add("d-inline-block", "mb-2", "text-primary");
+    strong_text.classList.add("mb-2", "text-primary", "mx-auto");
     strong_text.innerHTML = "MATCH " + (matchNumber + 1);
+
+
 
     let q_hr = document.createElement('hr');
     let r_hr = document.createElement('hr');
 
-    let lp_text = document.createElement("h3");
-    lp_text.innerHTML = "K/D/A : " + matchHistory20[matchNumber].kills + "/" + matchHistory20[matchNumber].deaths + "/" + matchHistory20[matchNumber].assists;
+    if (currMatch.victory){
+        div_row.classList.add("bg-success");
+        strong_text.innerHTML += " - VICTORY";
+    }
+    else {
+        div_row.classList.add("bg-danger");
+        strong_text.innerHTML += " - DEFEAT";
+    }
+
+    let kda_text = document.createElement("h3");
+    kda_text.innerHTML = "K/D/A : " + currMatch.kills + "/" + currMatch.deaths + "/" + currMatch.assists;
 
     let champ_played = document.createElement("h3");
-    champ_played = matchHistory20[matchNumber].champPlayed;
+    champ_played = currMatch.champPlayed;
 
     let side_text = document.createElement("h3");
-    side_text.innerHTML = matchHistory20[matchNumber].side;
-
+    side_text.innerHTML = currMatch.side;
 
 
     document.getElementById("matchhistory").append(div_col_outer);
     div_col_outer.append(div_row);
     div_col_outer.classList.add("container");
-    div_row.append(div_col_inner);
-    div_col_inner.append(r_hr, strong_text, q_hr, lp_text, champ_played, side_text);
+    div_row.append(r_hr, strong_text, q_hr, div_col_inner, div_col_middle);
+    div_col_inner.append(kda_text, champ_played, side_text);
+
+    var img = document.createElement("img");
+    img.src = "http://ddragon.leagueoflegends.com/cdn/12.17.1/img/champion/"+currMatch.champPlayed+".png";
+    img.classList.add("char");
+    div_col_middle.appendChild(img);
 }
 
