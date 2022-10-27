3dc1e3bb2a591050ddb5813d6ccc120268b71947
Colton Donkersgoed <coltonmdonk@gmail.com>
2022-10-26 20:10:19 -0400

    adjustments for cs50

--- quiz.js
+++ quiz.js
@@ -1,188 +1,79 @@
-// Initialization of global variables
-const questionArr = ["Which champion is primarily a top laner?", "Which champion is primarily a jungler?",
-                    "Which champion is primarily a mid laner?", "Which champion is primarily a bot laner?",
-                    "Which champion is primarily a support?"];
+<!DOCTYPE html>
+<html lang="en">
 
-const roles = {
-    "top": ["Tryndamere", "Akali", "Aatrox", "Cho'Gath", "Darius", "Fiora", "Gangplank", "Garen", "Illaoi", "Kled"],
-    "jungle": ["Kayn", "Nunu and Willump", "Elise", "Amumu", "Evelynn", "Gragas", "Graves", "Karthus", "Kindred", "Nidalee"],
-    "mid": ["Ryze", "Zed", "Azir", "Galio", "Cassiopeia", "Katarina", "Kassadin", "Qiyana", "Orianna", "Syndra"],
-    "bot": ["Jhin", "Draven", "Miss Fortune", "Ezreal", "Samira", "Sivir", "Ashe", "Kaisa", "Vayne", "Kog'Maw"],
-    "support": ["Sona", "Blitzcrank", "Thresh", "Pyke", "Zilean", "Yuumi", "Nami", "Janna", "Braum", "Leona"]
-};
+<head>
+    <meta charset="UTF-8">
+    <title>League of Legends - Quiz</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+    <link href="quiz_style.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <title>Trivia!</title>
 
-const regex_array = ["top", "jungle", "mid", "bot", "support"];
+</head>
 
-// Create function to generate a template for a MC button selection
+<body>
 
-const targetNode = document.getElementById('mc_container');
+    <!-- NAVBAR SECTION-->
+    <nav class="navbar navbar-expand-md sticky-top navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">
+                <img class="brand" src="brand.png" alt="L">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="main.html" class="btn btn-outline-primary">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="quiz.html" class="btn btn-primary disabled">QUIZ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="summoner_info.html" class="btn btn-outline-primary">RANKINGS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="personal_history.html" class="btn btn-outline-primary">SUMMONER SEARCH</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.html" class="btn btn-outline-primary">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-var score = 0;
-var total;
+    <div class="header">
+        <h1>League of Legends -- Trivia!</h1>
+    </div>
 
-function generate_mc() {
-    total = roles.top.length;
-    for (const [key, value] of Object.entries(roles)){
-        shuffle(value);
-    }
-    for (let i = 0; i < total; i++){
-         multipleChoiceTemplate(i);
-    };
-};
+    <!-- QUESTIONNAIRE CONTAINER -->
+    <div class="mc_container" id="mc_container">
+    </div>
+    <div class="container">
+        <div id="answer_cont" class="container" hidden>
+            <div class="section">
+                <h2> RESULTS </h2>
+                <hr>
+                <button id="refresh" onclick="refreshButtons()" hidden> Refresh </button>
+                <h4 id="answers" hidden></h4>
+            </div>
+        </div>
+    </div>
 
-// generate multiple choice section
-function multipleChoiceTemplate(counter) {
+<script src="quiz.js"></script>
 
-    let container_buttons = [];
-    let question_pick = getRndInteger(0, 4);
-
-    let q_con = document.createElement("container");
-    let q_sec = document.createElement("section");
-    let q_header = document.createElement("h2");
-    let q_hr = document.createElement('hr');
-    q_header.innerHTML = "QUESTION " + (counter + 1);
-    let q_h3 = document.createElement("h3");
-    q_h3.innerHTML = questionArr[question_pick];
-    q_h3.classList.add("q" + counter);
-    let q_h4 = document.createElement("h4");
-
-    targetNode.append(q_con);
-    q_con.append(q_sec);
-    q_con.classList.add("container");
-    q_sec.append(q_header);
-    q_sec.classList.add("section");
-    q_header.classList.add("h2");
-    q_sec.append(q_hr);
-    q_sec.append(q_h3);
-    q_sec.append(q_h4);
-
-    Object.keys(roles).forEach((tName) =>
-    {
-        let opt = document.createElement("button");
-        opt.classList.add("button");
-        opt.classList.add("btngrp" + counter);
-        opt.innerHTML = roles[tName][counter];
-        opt.setAttribute('name', roles[tName][counter]);
-        opt.onclick = function() {buttonLogic(opt, counter)};
-        container_buttons.push(opt);
-    })
-
-    shuffle(container_buttons);
-
-    for (const button of container_buttons){
-        q_sec.append(button);
-    }
-}
-
-
-
-// Function returns a random integer between min and max inclusive
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Function shuffles array; for randomizing question boxes
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
-
-// Logic for MC buttons
-function buttonLogic(target, id) {
-
-    var qTitle = document.getElementsByClassName("q" + id);
-    var buttons = document.getElementsByClassName("btngrp" + id);
-
-    target.style.background = "red";
-
-    for (const regex of regex_array) {
-        const reg = new RegExp(regex);
-        if (reg.test(qTitle[0].innerHTML) == true) {
-            if (roles[regex].includes(target.innerHTML)){
-                target.disabled = true;
-                target.style.background = "green";
-                score = score + 1;
-            }
-        }
-    };
+</body>
 
 
-    for (let i = 0; i <= buttons.length; i++){
-        buttons[i].disabled = true;
-        results();
-    }
-
-
-};
-
-// results function .. appears on all disabled
-function results() {
-    var all_disabled = true;
-    var list = []
-    $('[class]').each(function() {
-      this.classList.forEach(function(className) {
-        if (!className.indexOf('btngrp') && !list.includes(className)) {
-          list.push(className)
-        }
-      })
-    })
-
-    for (const item of list){
-        var elements = document.getElementsByClassName(item);
-
-        for (const element of elements) {
-            if (element.disabled != true)
-            {
-                all_disabled = false;
-            }
-        }
-    }
-
-    if (all_disabled == true){
-        document.getElementById("answer_cont").hidden = false;
-        document.getElementById("answers").innerHTML = score + "/10";
-        document.getElementById("answers").hidden = false;
-        document.getElementById("refresh").hidden = false;
-    }
-}
-
-// cleans window
-function clean_window(){
-    parent = document.getElementById("mc_container");
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-    all_disabled = false;
-    score = 0;
-    document.getElementById("answer_cont").hidden = true;
-    document.getElementById("answers").innerHTML = score + "/10";
-    document.getElementById("answers").hidden = true;
-    document.getElementById("refresh").hidden = true;
-    window.scrollTo({top: 0, behavior: 'smooth'});
-}
-
-// refreshes the mc .
-function refreshButtons(){
-    clean_window();
-    generate_mc();
-}
-
-
-window.addEventListener('load', (event) => {
-  generate_mc();
-});
-
-
+</html>
